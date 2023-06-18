10cfeb5cb1fde1716e6ad004a81f206ba9cb0557
Paul Fischer <me@paulmfischer.dev>
2023-06-18 16:10:57 -0500

    hook up config

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,22 +6,17 @@ using OpenLibraryApi.Configuration;
 IConfiguration config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
+var apiConfiguration = new ApiConfiguration(config.GetValue<string>("SearchApi"));
+Console.WriteLine($"checking api config: {apiConfiguration.SearchApi}");
 
 using IHost host = Host
     .CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
-        // services.AddTransient<ISearchService, SearchService>();
+        services.AddSingleton(apiConfiguration);
         services.UseOpenLibraryApi();
-        // var myConfigurationSection = configuration.GetSection("app");
-
-        // services.AddSingleton<IValidateOptions<AppOptions>, AppOptionsValidator>();
-        // services.Configure<AppOptions>(myConfigurationSection);
     })
     .Build();
-
-    
-// var apiConfiguration = new ApiConfiguration(config.GetValue<string>("SearchApi"));
 
 Console.Write("Enter a search term: ");
 var searchTerm = Console.ReadLine();
@@ -29,5 +24,3 @@ ArgumentException.ThrowIfNullOrEmpty(searchTerm, nameof(searchTerm));
 
 ISearchService searchService = host.Services.GetService<ISearchService>() ?? throw new NullReferenceException("ISearch Service not registered correctly.");
 await searchService.Search(searchTerm);
-
-// host.Run(); 
--- OpenLibraryApi/API/SearchService.cs
+++ OpenLibraryApi/API/SearchService.cs
@@ -1,17 +1,18 @@
 using Microsoft.Extensions.Configuration;
+using OpenLibraryApi.Configuration;
 
 public class SearchService : ISearchService
 {
-    private readonly IConfiguration configuration;
+    private readonly ApiConfiguration configuration;
 
-    public SearchService(IConfiguration configuration)
+    public SearchService(ApiConfiguration configuration)
     {
         this.configuration = configuration;
     }
     
     public Task Search(string searchTerm)
     {
-        Console.WriteLine($"search api config setting {this.configuration.GetValue<string>("SearchAPI")}");
+        Console.WriteLine($"search api config setting {this.configuration.SearchApi}");
         Console.WriteLine($"Searching Open API for {searchTerm}");
         return Task.CompletedTask;
     }
